fad89007163d42dad9413734a1d68af1480bdef8
CataeroGong <CataeroGong@hotmail.com>
2023-08-26 20:11:55 +0800

    v0.1

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # SimpleTextReaderServer
 简单的 SimpleTextReader Web Server，并添加一些新功能
+
+## 【写在前面】本人电脑重度用户，所以只针对“界面版”做修改
+
+这两天又努了一把力，增加了读取服务器上文件的功能 :smile: 还有其他一些小改动
+
+本来是想用 Python 给写个服务，然后提供 HTTP 服务、文件下载和记录进度的功能，写得都差不多了，结果写着写着脑子一抽，想 javascript 可以直接访问 WebDAV 的吧？因为我日常家里就开着一个简易的 WebDAV 服务，可以的话，这样就不用多跑一个服务了，WebDAV 既当 HTTP 服务器又当文件服务器。试了试，还真有戏 :wink: 
+
+不过这样一改，就需要有自己的 WebDAV 服务器，我自己用的是 https://github.com/hacdias/webdav ，很简单很好用 :+1: :+1: :+1:
+
+其实，我是把 WebDAV 的目录放在了 OneDrive 里，这样家里和公司里都同步了 :joy:
+
+## 【改动】
+
+* 对原来的 SimpleTextReader 完全不改动，直接在外面套了个页面，然后从外层网页 hack :sunglasses:
+
+* 需要把 SimpleTextReader 、增强脚本和小说都放在 WevDAV 服务器上，books 目录要开写入权限，阅读进度和小说文件是放在一起的。
+    ```
+    webroot    （WebDAV 根目录）
+      - index.html （外层主页面）
+      - books  （小说目录）
+        - 小说.txt
+        - 小说.txt.progress    （小说阅读进度记录文件，每个小说一个）
+        ...
+      - scripts    （增强脚本目录）
+        - mod.js
+        - webdav.js
+      - SimpleTextReader    （STR 原始目录）
+        ...
+    ```
+
+* 可以列出 WebDAV 服务器特定目录下的 txt 小说。
+
+* 可以读取 WebDAV 服务器上 txt 小说。
+
+* 服务器上小说的阅读进度也存放在服务器上，换浏览器、换电脑也可以保持进度。
+
+* 如果是读服务器上小说的话，下次打开页面会自动打开小说，不需要再手动打开。
+
+* 顺便做了打开外部 txt 链接的功能，不过这个就和本机 txt 的功能基本一样了。
+
+* 分页条还是太碍事，增加了透明度参数 :smile:
+
+## 【bugfix】
+
+*  ui.js 里有一个地方把 parent 给误修改了，我没直接改源代码，在外层页面里打了补丁
+    ```js
+    // SimpleTextReader/scripts/ui.js (line:20)
+    // window.parent 被误修改了
+    // 应该 let emInPx = getSizePrecise('1em', contentContainer);
+    let emInPx = getSizePrecise('1em', parent=contentContainer);
+    ```
+## 【感谢】
+
+* WebDAV 功能使用了 webdavjs 库： https://github.com/aslakhellesoy/webdavjs
--- scripts/mod.js
+++ scripts/mod.js
@@ -1,33 +1,57 @@
+document.styleSheets[0].insertRule(`.icon-btn {
+	cursor: pointer;
+	position: fixed;
+	right: 20px;
+	width: 30px;
+	height: 30px;
+	padding: 4px;
+	z-index: 1005;
+	color: var(--mainColor_focused);
+}`);
+document.styleSheets[0].insertRule(`dialog {
+	border-radius: 10px;
+	font-family: ui;
+	font-size: var(--p_fontSize);
+	background: var(--bgColor);
+	color: var(--fontColor);
+}`);
+document.styleSheets[0].insertRule(`dialog::backdrop {background-color: rgba(0,0,0,0.5)}`);
+document.styleSheets[0].insertRule(`dialog .dlg-close-btn {
+	cursor: pointer;
+	float: right;
+	padding: 2px;
+	border: 1px solid;
+	border-radius: 5px;
+}`);
+document.styleSheets[0].insertRule(`dialog span.dlg-body {
+	display: block;
+	overflow: auto;
+	margin: 10px 20px;
+	padding: 5px;
+}`);
+document.styleSheets[0].insertRule(`dialog span.dlg-body input {
+	float: right;
+	font-family: ui;
+	font-size: 1em;
+	background: var(--bgColor);
+	color: var(--fontColor);
+}`);
+document.styleSheets[0].insertRule(`dialog span.dlg-body button {
+	font-family: ui;
+	padding: 2px 10px;
+	border: 1px solid;
+	border-radius: 5px;
+	font-size: 1em;
+	background: var(--bgColor);
+	color: var(--fontColor);
+}`);
+document.styleSheets[0].insertRule(`#pagination:hover {opacity: 1;}`);
+
+const func_keydown_ = document.onkeydown; // 保存页面原来的 onkeydown 函数，下面会临时屏蔽 onkeydown
+
 // ------------------------------------------------
 // Settings
 // ------------------------------------------------
-
-let pagination_bottom_default = getCSS("#pagination", "bottom");
-let p_lineHeight_default = getCSS(":root", "--p_lineHeight");
-let p_fontSize_default = getCSS(":root", "--p_fontSize");
-let light_fontColor_default = getCSS(":root", "--fontColor");
-let light_bgColor_default = getCSS(":root", "--bgColor");
-let dark_fontColor_default = getCSS('[data-theme="dark"]', "--fontColor");
-let dark_bgColor_default = getCSS('[data-theme="dark"]', "--bgColor");
-
-let pagination_bottom;
-let p_lineHeight;
-let p_fontSize;
-let light_fontColor;
-let light_bgColor;
-let dark_fontColor;
-let dark_bgColor;
-
-// load settings
-function loadSettings() {
-	pagination_bottom = localStorage.getItem("pagination_bottom") || pagination_bottom_default;
-	p_lineHeight = localStorage.getItem("p_lineHeight") || p_lineHeight_default;
-	p_fontSize = localStorage.getItem("p_fontSize") || p_fontSize_default;
-	light_fontColor = localStorage.getItem("light_fontColor") || light_fontColor_default;
-	light_bgColor = localStorage.getItem("light_bgColor") || light_bgColor_default;
-	dark_fontColor = localStorage.getItem("dark_fontColor") || dark_fontColor_default;
-	dark_bgColor = localStorage.getItem("dark_bgColor") || dark_bgColor_default;
-}
 
 function getCSS(sel, prop) {
 	for (const sheet of document.styleSheets) {
@@ -39,95 +63,135 @@ function getCSS(sel, prop) {
 	}
 	return null;
 }
-
-function setCSS(sel, prop, val, def) {
+function setCSS(sel, prop, val, defVal) {
 	for (const sheet of document.styleSheets) {
 		for (const rule of sheet.cssRules) {
 			if (rule.selectorText === sel) {
-				rule.style.setProperty(prop, val?val:def);
-				console.log(sel + " { " + prop + " : " + (val?val:def) + " }");
+				rule.style.setProperty(prop, val?val:defVal);
+				// console.log(sel + " { " + prop + " : " + (val?val:defVal) + " }");
 			}
 		}
 	}
 }
 
+let p_lineHeight_default = getCSS(":root", "--p_lineHeight");
+let p_fontSize_default = getCSS(":root", "--p_fontSize");
+let light_fontColor_default = getCSS(":root", "--fontColor");
+let light_bgColor_default = getCSS(":root", "--bgColor");
+let dark_fontColor_default = getCSS('[data-theme="dark"]', "--fontColor");
+let dark_bgColor_default = getCSS('[data-theme="dark"]', "--bgColor");
+let pagination_bottom_default = getCSS("#pagination", "bottom");
+let pagination_opacity_default = "1";
+
+let p_lineHeight;
+let p_fontSize;
+let light_fontColor;
+let light_bgColor;
+let dark_fontColor;
+let dark_bgColor;
+let pagination_bottom;
+let pagination_opacity;
+
+// load settings
+function loadSettings() {
+	p_lineHeight = localStorage.getItem("p_lineHeight") || p_lineHeight_default;
+	p_fontSize = localStorage.getItem("p_fontSize") || p_fontSize_default;
+	light_fontColor = localStorage.getItem("light_fontColor") || light_fontColor_default;
+	light_bgColor = localStorage.getItem("light_bgColor") || light_bgColor_default;
+	dark_fontColor = localStorage.getItem("dark_fontColor") || dark_fontColor_default;
+	dark_bgColor = localStorage.getItem("dark_bgColor") || dark_bgColor_default;
+	pagination_bottom = localStorage.getItem("pagination_bottom") || pagination_bottom_default;
+	pagination_opacity = localStorage.getItem("pagination_opacity") || pagination_opacity_default;
+}
+
 function applySettings() {
-	setCSS("#pagination", "bottom", pagination_bottom, pagination_bottom_default);
 	setCSS(":root", "--p_lineHeight", p_lineHeight, p_lineHeight_default);
 	setCSS(":root", "--p_fontSize", p_fontSize, p_fontSize_default);
 	setCSS(":root", "--fontColor", light_fontColor, light_fontColor_default);
 	setCSS(":root", "--bgColor", light_bgColor, light_bgColor_default);
 	setCSS('[data-theme="dark"]', "--fontColor", dark_fontColor, dark_fontColor_default);
 	setCSS('[data-theme="dark"]', "--bgColor", dark_bgColor, dark_bgColor_default);
+	setCSS("#pagination", "bottom", pagination_bottom, pagination_bottom_default);
+	setCSS("#pagination", "opacity", pagination_opacity, pagination_opacity_default);
 }
 
-function showSettings() {
+function showSettingDlg() {
 	$(`
-<div id="settings" style="position:fixed;right:20px;bottom:70px;z-index:1006;background: var(--mainColor);border:1px darkgray solid;border-radius:10px;padding:10px;">
+<dialog id="settingDlg">
+	<div><span class="material-symbols-rounded dlg-close-btn" onclick="hideSettingDlg();"> close </span></div>
+	<span class="dlg-body">
 	<div>
-		<span style="display:inline-block;width:10rem">行高：</span>
-		<input type="text" size="10" id="setting_p_lineHeight" value="${p_lineHeight}" />
-	</div>
-	<div>
-		<span style="display:inline-block;width:10rem">字号：</span>
-		<input type="text" size="10"  id="setting_p_fontSize" value="${p_fontSize}" />
+		<span>行高：</span>
+		<input type="text" size="10" style="float:right" id="setting_p_lineHeight" value="${p_lineHeight}" />
 	</div>
 	<div>
-		<span style="display:inline-block;width:10rem">日间字符色：</span>
-		<input type="text" size="10"  id="setting_light_fontColor" value="${light_fontColor}" />
+		<span>字号：</span>
+		<input type="text" size="10" id="setting_p_fontSize" value="${p_fontSize}" />
 	</div>
 	<div>
-		<span style="display:inline-block;width:10rem">日间背景色：</span>
-		<input type="text" size="10"  id="setting_light_bgColor" value="${light_bgColor}" />
+		<span>日间字符色：</span>
+		<input type="text" size="10" id="setting_light_fontColor" value="${light_fontColor}" />
 	</div>
 	<div>
-		<span style="display:inline-block;width:10rem">夜间字符色：</span>
-		<input type="text" size="10"  id="setting_dark_fontColor" value="${dark_fontColor}" />
+		<span>日间背景色：</span>
+		<input type="text" size="10" id="setting_light_bgColor" value="${light_bgColor}" />
 	</div>
 	<div>
-		<span style="display:inline-block;width:10rem">夜间背景色：</span>
-		<input type="text" size="10"  id="setting_dark_bgColor" value="${dark_bgColor}" />
+		<span>夜间字符色：</span>
+		<input type="text" size="10" id="setting_dark_fontColor" value="${dark_fontColor}" />
 	</div>
 	<div>
-		<span style="display:inline-block;width:10rem">分页条与底部距离：</span>
-		<input type="text" size="10"  id="setting_pagination_bottom" value="${pagination_bottom}" />
+		<span>夜间背景色：</span>
+		<input type="text" size="10" id="setting_dark_bgColor" value="${dark_bgColor}" />
 	</div>
-	<div style="padding:4px;">
-		<button onclick="removeAllHistory();loadSettings();applySettings();hideSettings();" style="color:red;">清空设置&阅读历史</button>
-		<button onclick="saveSettings();">应用</button>
-		<button onclick="hideSettings();">取消</button>
+	<div>
+		<span>分页条与底部距离：</span>
+		<input type="text" size="10" id="setting_pagination_bottom" value="${pagination_bottom}" />
 	</div>
-</div>
-`).insertAfter("#switch-btn");
+	<div>
+		<span>分页条透明度：</span>
+		<input type="text" size="10" id="setting_pagination_opacity" value="${pagination_opacity}" />
+	</div>
+	<div style="padding:4px;margin-top:10px;">
+		<button onclick="removeAllHistory();loadSettings();applySettings();hideSettingDlg();" style="color:var(--mainColor_focused);">！清空设置&阅读历史！</button>
+		<button onclick="saveSettings();applySettings();hideSettingDlg();" style="float:right;">应用</button>
+	</div>
+	</span>
+</dialog>
+`).bind("cancel", hideSettingDlg).insertAfter("#switch-btn");
+	document.getElementById("settingDlg").showModal();
+	// document.getElementById("settingDlg").
+	document.onkeydown = null;
 }
 
 function saveSettings() {
-	pagination_bottom = $("#setting_pagination_bottom").val() || pagination_bottom_default;
 	p_lineHeight = $("#setting_p_lineHeight").val() || p_lineHeight_default;
 	p_fontSize = $("#setting_p_fontSize").val() || p_fontSize_default;
 	light_fontColor = $("#setting_light_fontColor").val() || light_fontColor_default;
 	light_bgColor = $("#setting_light_bgColor").val() || light_bgColor_default;
 	dark_fontColor = $("#setting_dark_fontColor").val() || dark_fontColor_default;
 	dark_bgColor = $("#setting_dark_bgColor").val() || dark_bgColor_default;
-	localStorage.setItem("pagination_bottom", pagination_bottom);
+	pagination_bottom = $("#setting_pagination_bottom").val() || pagination_bottom_default;
+	pagination_opacity = $("#setting_pagination_opacity").val() || pagination_opacity_default;
 	localStorage.setItem("p_lineHeight", p_lineHeight);
 	localStorage.setItem("p_fontSize", p_fontSize);
 	localStorage.setItem("light_fontColor", light_fontColor);
 	localStorage.setItem("light_bgColor", light_bgColor);
 	localStorage.setItem("dark_fontColor", dark_fontColor);
 	localStorage.setItem("dark_bgColor", dark_bgColor);
-	applySettings();
-	hideSettings();
+	localStorage.setItem("pagination_bottom", pagination_bottom);
+	localStorage.setItem("pagination_opacity", pagination_opacity);
 }
-function hideSettings() {
-	$("#settings").remove();
+function hideSettingDlg() {
+	$("#settingDlg").remove();
+	document.onkeydown = func_keydown_;
 }
 
 $(`
-<div style="position:fixed;bottom:70px;right:20px;width:30px;height:30px;padding:4px;z-index:1005;color:var(--mainColor_focused);cursor:pointer;" onclick="showSettings();">
+<div class="icon-btn" style="bottom:70px;">
 	<span class="material-symbols-rounded"> settings </span>
 </div>
-`).insertBefore("#switch");
+`).click(showSettingDlg).insertBefore("#switch");
 
 loadSettings();
 applySettings();
@@ -143,6 +207,7 @@ function openLink(link) {
 	xhr.responseType = "blob";
 	xhr.onload = function() {
 		// console.log(this.response);
+		resetVars();
 		this.response.name = decodeURI(link);
 		handleSelectedFile([this.response]);
 	}
@@ -158,8 +223,8 @@ function showOpenLink() {
 }
 
 $(`
-<div style="position:fixed;bottom:120px;right:20px;width:30px;height:30px;padding:4px;z-index:1005;color:var(--mainColor_focused);cursor:pointer;">
-	<span class="material-symbols-rounded"> add_link </span>
+<div class="icon-btn" style="bottom:120px;">
+	<span class="material-symbols-rounded"> link </span>
 </div>
 `).insertBefore("#switch").click(showOpenLink);
 
@@ -168,17 +233,24 @@ $(`
 // Open file on server
 // ------------------------------------------------
 
-let strs_server = "";
-let strs_tag = "<STRSvr>";
+const strs_server = ""; // "http://localhost:8001";
+const strs_tag = "☁|";
+const strs_file_item = "STRS_FILE";
 
-function openFileOnServer(filename) {
-	let link = strs_server + "/books/" + filename;
+let strs_file = localStorage.getItem(strs_file_item);
+let strs_file_line = ""; // strs_tag + filename + ":" + line
+
+function openFileOnServer(fname) { // fname: 不带 strs_tag 的文件名
+	let link = strs_server + "/books/" + fname;
 	let xhr = new XMLHttpRequest();
 	xhr.open("get", link, true);
+	xhr.setRequestHeader("If-Modified-Since","0"); // 强制刷新，不使用缓存
 	xhr.responseType = "blob";
 	xhr.onload = function() {
 		// console.log(this.response);
-		this.response.name = strs_tag + filename;
+		resetVars();
+		loadProgressFromServer(fname);
+		this.response.name = strs_tag + fname;
 		handleSelectedFile([this.response]);
 	}
 	xhr.send();
@@ -186,95 +258,129 @@ function openFileOnServer(filename) {
 }
 
 function showOpenFileOnServerDlg() {
-	$.getJSON(strs_server + "/books").done(data => {
-		let book_list = "";
-		data.books.forEach((v) => {
-			book_list += `<div style="cursor:pointer" onclick="openFileOnServer('${v}');$('#OpenFileOnServerDlg').remove();">${v}</div>`;
-		});
-		$(`<dialog id="OpenFileOnServerDlg" style="width:600px;height:500px">
-			${book_list}
-			</dialog>`).insertAfter("#switch-btn");
-		document.getElementById("OpenFileOnServerDlg").showModal();
-	});
+	let fs = WebDAV.Fs(strs_server);
+	let dir = fs.dir("/books");
+	let lst = dir.children();
+	let fname_list = [];
+	for (const f of lst) {
+		let fname = decodeURIComponent(f.name);
+		if (fname.substring(fname.length - 4).toLowerCase() == ".txt")
+			fname_list.push([fname, ""]);
+	}
+	for (const f of lst) {
+		let fname = decodeURIComponent(f.name);
+		if (fname.substring(fname.length - 9).toLowerCase() == ".progress") {
+			fname = fname.substring(0, fname.length - 9);
+			let book = fname_list.find((e) => e[0].toLowerCase() == fname.toLowerCase());
+			if (book)
+				book[1] = '<span class="material-symbols-rounded" style="vertical-align:bottom;"> clock_loader_40 </span>' + f.read();
+		}
+	}
+	fname_list.sort((a, b) =>
+		((a[1] && b[1]) || (!a[1] && !b[1]))
+			? (a[0].localeCompare(b[0], "zh"))
+			: (a[1] ? -1 : 1));
+	// console.log(fname_list);
+	let book_list = "";
+	for (const f of fname_list) {
+		book_list += `<div style="cursor:pointer;border:1px gray dotted;padding:2px 3px;margin:5px 0px;" onclick="openFileOnServer('${f[0]}');hideOpenFileOnServerDlg();">
+		<span>${f[0]}</span>&nbsp;<span style="opacity:0.3;float:right;">${f[1]}</span>
+		</div>`;
+	}
+	$(`<dialog id="OpenFileOnServerDlg">
+		<div><span class="material-symbols-rounded dlg-close-btn" onclick="hideOpenFileOnServerDlg();"> close </span></div>
+		<span class="dlg-body" style="height: 400px;">
+		${book_list}
+		</span>
+		</dialog>`).insertAfter("#switch-btn");
+	document.getElementById("OpenFileOnServerDlg").showModal();
+	document.getElementById("OpenFileOnServerDlg").addEventListener("cancel", () => hideOpenFileOnServerDlg());
+	document.onkeydown = null;
+}
+
+function hideOpenFileOnServerDlg() {
+	$('#OpenFileOnServerDlg').remove();
+	document.onkeydown = func_keydown_;
 }
 
 $(`
-<div style="position:fixed;bottom:170px;right:20px;width:30px;height:30px;padding:4px;z-index:1005;color:var(--mainColor_focused);cursor:pointer;">
-	<span class="material-symbols-rounded"> folder_open </span>
+<div class="icon-btn" style="bottom:170px;">
+	<span class="material-symbols-rounded"> cloud </span>
 </div>
 `).insertBefore("#switch").click(showOpenFileOnServerDlg);
 
-// hack functions
-let strs_file_line = "";
-function setProgressToServer() {
-	if (!filename)
-		return;
-	let line = getTopLineNumber();
-	if ((filename + ":" + line) == strs_file_line)
-		return;
-	console.log("setProgressToServer(): " + filename.substring(strs_tag.length) + ":" + line);
-	$.ajax({
-		method: "PUT",
-		url: strs_server + "/progress/" + filename.substring(strs_tag.length),
-        contentType: "application/json",
-        data: JSON.stringify(line)
-	}).done(resp => {
-		console.log(resp);
-		strs_file_line = filename + ":" + line;
-	}).fail(err => {
-		console.log(err);
-	})
+function saveProgressToServer() {
+	if ((filename) && (filename.substring(0, strs_tag.length) == strs_tag)) { // file on server
+		if (contentContainer.style.display == "none") { // 阅读区域不可见，说明可能正在drag，getTopLineNumber()会取到错误行数，应该跳过
+			// console.log("skip");
+			return;
+		}
+		let line = getTopLineNumber(filename);
+		if ((filename+":"+line) != strs_file_line) {
+			console.log("saveProgressToServer: " + filename + ":" + line);
+			localStorage.setItem(strs_file_item, filename.substring(strs_tag.length));
+			let prog_file = WebDAV.Fs(strs_server).file("/books/" + filename.substring(strs_tag.length) + ".progress");
+			prog_file.write(line);
+			strs_file_line = filename + ":" + line;
+		}
+	} else { // local file
+		localStorage.removeItem(strs_file_item);
+		strs_file_line = "";
+	}
 }
-setInterval(setProgressToServer, 1000);
-// let hack_setHistory = setHistory.toString() + `
-// //console.log("hack_setHistory(): " + filename + ":" + lineNumber);
-// if ((filename.substring(0, strs_tag.length) != strs_tag)) {
-// 	setHistory(filename, lineNumber);
-// }
-// `;
-// window["setHistory"] = new Function("filename, lineNumber", hack_setHistory);
 
+function loadProgressFromServer(fname) { // fname: 不带 strs_tag 的文件名
+	console.log("loadProgressFromServer: " + fname);
+	let line = 0;
+	let dir = WebDAV.Fs(strs_server).dir("/books");
+	for (const f of dir.children()) {
+		// console.log(f);
+		if (decodeURIComponent(f.name).toLocaleLowerCase() == (fname.toLocaleLowerCase() + ".progress")) {
+			try {
+				line = parseInt(f.read());
+			} catch (e) {
+				console.log("Err:" + e);
+				line = 0;
+			}
+			break;
+		}
+	}
+	setHistory(strs_tag + fname, line);
+	localStorage.setItem(strs_file_item, fname);
+	strs_file_line = strs_tag + fname + ":" + line;
+	// console.log("strs_file_line:" + strs_file_line);
+	return line;
+}
+
+function strs_worker() { // 定时将当前书在 localStorage 里的进度保存到服务器上
+	saveProgressToServer();
+	window.parent.document.title = document.title;
+	setTimeout(strs_worker, 1000);
+}
+
+// hack functions
+function get_func_args(func) {
+	let re = /^\s*function\s*\w*\s*\(([^\)]*)\)[^{]*{.*}\s*$/si;
+	let r = re.exec(func.toString());
+	return r ? r[1] : "";
+}
 function get_func_body(func) {
 	let re = /^\s*function\s*\w*\s*\([^\)]*\)[^{]*{(.*)}\s*$/si;
 	let r = re.exec(func.toString());
-	if (r)
-		return r[1];
-	else
-		return "";
+	return r ? r[1] : "";
+}
+function replace_func(func_owner, func_name, func_copyname, new_func) {
+	func_owner[func_copyname] = new Function(get_func_args(func_owner[func_name]), get_func_body(func_owner[func_name]));
+	func_owner[func_name] = new Function(get_func_args(func_owner[func_name]), get_func_body(new_func));
 }
 
-window["setHistory_"] = new Function("filename, lineNumber", get_func_body(setHistory));
-window["setHistory"] = new Function("filename, lineNumber",
-`// console.log("HACK setHistory(): " + filename + ":" + lineNumber);
-if ((filename.substring(0, strs_tag.length) != strs_tag)) {
-	setHistory_(filename, lineNumber);
-}`);
+replace_func(WebDAV, "request", "request____copy", function () {
+	headers["If-Modified-Since"] = "0"; // 强制刷新，不使用缓存
+	// console.log(headers);
+	return this.request____copy(verb, url, headers, data, type, callback);
+});
 
-async function getProgressFromServer(filename) {
-	console.log("getProgressFromServer(): " + filename);
-	let line = 0
-	$.ajax({
-		async: false,
-		method: "GET",
-		url: strs_server + "/progress/" + filename,
-        contentType: "application/json"
-	}).done(resp => {
-		line = parseInt(resp.line);
-	});
-	if (line) {
-		console.log("Goto line: " + line);
-		let success = gotoLine(line, false);
-        if (success === -1) {
-            line = 0;
-        }
-	}
-	return line;
+if (strs_file) {
+	openFileOnServer(strs_file);
 }
-window["getHistory_"] = new Function("filename", get_func_body(getHistory));
-window["getHistory"] = new Function("filename,",
-`//console.log("HACK getHistory(): " + filename);
-if ((filename.substring(0, strs_tag.length) == strs_tag)) {
-	return getProgressFromServer(filename.substring(strs_tag.length));
-} else {
-	return getHistory_(filename);
-}`);
+strs_worker();
